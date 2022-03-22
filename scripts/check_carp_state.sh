54f0c8f4846ca3299123b555b3cad3746a906497
MrClayPole <9336574+MrClayPole@users.noreply.github.com>
2022-03-22 13:29:56 +0000

    Scrip to check if carp/HA is enabled
    This will check if HA is enabled and if the firewall is MASTER or BACKUP

---
+++ scripts/check_carp_state.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# Script to check if firewall is in high availability mode and if its backup or master
+#
+# Possible returns
+#   DISBALE: HA is disabled
+#   MASTER: HA is enable and this firewall is the MASTER
+#   BACKUP: HA is enabled and this firewall is the BACKUP
+
+
+# Init variables
+carp_state="UNKNOWN"
+
+# Test for CARP interfaces
+ifconfig -a | grep 'carp:' > /dev/null
+if [ $? = 0 ]; then
+  ifconfig -a | grep 'carp:' | grep BACKUP > /dev/null
+  if [ $? = 0 ]; then
+    carp_state="BACKUP"
+  else
+    ifconfig -a | grep 'carp:' | grep MASTER > /dev/null
+    if [ $? = 0 ]; then
+      carp_state="MASTER"
+    fi
+  fi
+else
+  carp_state="DISABLED"
+fi
+
+# Print carp state
+echo $carp_state
